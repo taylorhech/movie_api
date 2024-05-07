4a3aa90f198f0df9c46db27eb8af3decd59d66a8
taylorhech <114107307+taylorhech@users.noreply.github.com>
2024-05-07 13:58:37 -0700

    Created URL endpoints & request methods
    Updated movie data to correspond with the URL endpoints & request methods

--- index.js
+++ index.js
@@ -9,107 +9,121 @@ app.use(bodyParser.json()); //middleware application for body-parser
 //Logging middleware morgan
 app.use(morgan('common'));
 
-let topTenMovies = [
+let movies = [
     {
-        title: 'Your Name',
-        director: 'Makoto Shinkai',
-        genre: 'Fantasy/Romance',
-        year: 2016
+        Title: 'Your Name',
+        Director: {
+            Name: 'Makoto Shinkai',
+            Bio: 'Makoto Niitsu, known as Makoto Shinkai, is a Japanese filmmaker and novelist know for his anime feature films.',
+            Birth: 1973
+        },
+        Genre: {
+            Name: 'Fantasy',
+            Description: 'Fantasy films include fantastic themes, usually magic, supernatural events, mythology, folklore, or exoctic fantasy worlds.'
+        },
+        Year: 2016
     },
     {
-        title: 'Past Lives',
-        director: 'Celine Song',
-        genre: 'Romance/Drama',
-        year: 2023
+        Title: 'Past Lives',
+        Director: {
+            Name: 'Celine Song',
+            Bio: 'Celine Song is a Korean-Canadian director, playwright, and screenwriter, receiving critical acclaim for her directorial film debut of Past Lives.',
+            Birth: 1988
+        },
+        Genre: {
+            Name: 'Drama',
+            Description: 'In film and television, drama is a category narrative fiction (or semi-fiction) intended to be more serious than humurous in tone.'
+        },
+        Year: 2023
     },
     {
-        title: 'Coco',
-        director: 'Adrian Molina, Lee Unkrich',
-        genre: 'Fantasy/Adventure',
-        year: 2017
+        Title: 'Coco',
+        Director: [ 
+            {
+            Name: 'Adrian Molina',
+            Bio: 'Adrian Molina is an American animator, storyboad artist, and screenwriter who works for Pixar.',
+            Birth: 1985
+        }, {
+            Name: 'Lee Unkrich',
+            Bio: 'Lee Edward Unkrich is an American film director, editor, and writer best known for his work with animation studio, Pixar.',
+            Birth: 1967
+        }],  
+        Genre: {
+            Name: 'Fantasy',
+            Description: 'Fantasy films include fantastic themes, usually magic, supernatural events, mythology, folklore, or exoctic fantasy worlds.'
+        },
+        Year: 2017
     },
     {
-        title: 'Ice Age: Dawn of the Dinosaurs',
-        director: 'Carlos Saldanha',
-        genre: 'Adventure/Comedy',
-        year: 2009
+        Title: 'Ice Age: Dawn of the Dinosaurs',
+        Director: {
+            Name: 'Carlos Saldanha',
+            Bio: 'Carlos Saldanha is a Brazilian animator, director, producer, and voice actor of animated films who worked with Blue Sky Studios until its closer in 2021.',
+            Birth: 1965
+        },
+        Genre: {
+            Name: 'Comedy',
+            Description: 'In film, the comedy category emphasizes humor, is designed to amuse audiences, and make them laugh.'
+        },
+        Year: 2009
     },
     {
-        title: 'Bullet Train',
-        director: 'David Leitch',
-        genre: 'Action/Comedy',
-        year: 2022
+        Title: 'Bullet Train',
+        Director: {
+            Name: 'David Leitch',
+            Bio: 'David Leitch is an American filmmaker, stunt performer, stunt coordinator, and actor who made his directorial debut on the 2014 action film John Wick.',
+            Birth: 1975
+        },
+        Genre: {
+            Name: 'Comedy',
+            Description: 'In film, the comedy category emphasizes humor, is designed to amuse audiences, and make them laugh.'
+        },
+        Year: 2022
     },
     {
-        title: 'Spider-Man: Across the Spider-Verse',
-        director: 'Joaquim Dos Santos, Kemp Powers, Justin K. Thompson',
-        genre: 'Action/Comedy',
-        year: 2023
+        Title: 'Spider-Man: Across the Spider-Verse',
+        Director: 'Joaquim Dos Santos, Kemp Powers, Justin K. Thompson',
+        Genre: 'Action',
+        Year: 2023
     },
     {
-        title: 'How to Train Your Dragon',
-        director: 'Dean DeBlois, Chris Sanders',
-        genre: 'Family/Fantasy',
-        year: 2010
+        Title: 'How to Train Your Dragon',
+        Director: 'Dean DeBlois, Chris Sanders',
+        Genre: {
+            Name: 'Fantasy',
+            Description: 'Fantasy films include fantastic themes, usually magic, supernatural events, mythology, folklore, or exoctic fantasy worlds.'
+        },
+        Year: 2010
     },
     {
-        title: 'Howl\'s Moving Castle',
-        director: 'Hayao Miyazaki',
-        genre: 'Fantasy/Adventure',
-        year: 2004
+        Title: 'Howl\'s Moving Castle',
+        Director: 'Hayao Miyazaki',
+        Genre: {
+            Name: 'Fantasy',
+            Description: 'Fantasy films include fantastic themes, usually magic, supernatural events, mythology, folklore, or exoctic fantasy worlds.'
+        },
+        Year: 2004
     },
     {
-        title: 'Rush Hour',
-        director: 'Brett Ratner',
-        genre: 'Comedy/Action',
-        year: 1998
+        Title: 'Rush Hour',
+        Director: 'Brett Ratner',
+        Genre: {
+            Name: 'Comedy',
+            Description: 'In film, the comedy category emphasizes humor, is designed to amuse audiences, and make them laugh.'
+        },
+        Year: 1998
     },
     {
-        title: 'Coherence',
-        director: 'James Ward Byrkit',
-        genre: 'Sci-fi/Thriller',
-        year: 2013
+        Title: 'Coherence',
+        Director: 'James Ward Byrkit',
+        Genre: {
+            Name: 'Sci-fi',
+            Description: 'The science fiction genre uses speculative, fictional science-based depictions of phenomena that are not fully accepted by mainstream science, such as extraterrestrial lifeforms, spacecraft, robots, cyborgs, mutants, interstellar travel, time travel, or other technologies.'
+        },
+        Year: 2013
     },
 ];
 
-let directors = [
-    {
-        name: 'Makoto Shinkai',
-        bio: 'Makoto Niitsu, known as Makoto Shinkai, is a Japanese filmmaker and novelist know for his anime feature films.',
-        birthYear: 1973,
-        deathYear: 'N/A'
-    },
-    {
-        name: 'Celine Song',
-        bio: 'Celine Song is a Korean-Canadian director, playwright, and screenwriter, receiving critical acclaim for her directorial film debut of Past Lives.',
-        birthYear: 1988,
-        deathYear: 'N/A'
-    },
-    {
-        name: 'Adrian Molina',
-        bio: 'Adrian Molina is an American animator, storyboad artist, and screenwriter who works for Pixar.',
-        birthYear: 1985,
-        deathYear: 'N/A'
-    },
-    {
-        name: 'Lee Unkrich',
-        bio: 'Lee Edward Unkrich is an American film director, editor, and writer best known for his work with animation studio, Pixar.',
-        birthYear: 1967,
-        deathYear: 'N/A'
-    },
-    {
-        name: 'Carlos Saldanha',
-        bio: 'Carlos Saldanha is a Brazilian animator, director, producer, and voice actor of animated films who worked with Blue Sky Studios until its closer in 2021.',
-        birthYear: 1965,
-        deathYear: 'N/A'
-    },
-    {
-        name: 'David Leitch',
-        bio: 'David Leitch is an American filmmaker, stunt performer, stunt coordinator, and actor who made his directorial debut on the 2014 action film John Wick.',
-        birthYear: 1975,
-        deathYear: 'N/A'
-    }
-];
 
 let users = [
     {
@@ -164,7 +178,7 @@ app.put('/users/:id', (req, res) => {
 })
 
 //CREATE- adds a movie to user's list of favorites
-app.put('/users/:id/:movieTitle', (req, res) => {
+app.post('/users/:id/:movieTitle', (req, res) => {
     const { id, movieTitle } = req.params;
 
     let user = users.find( user => user.id == id);
@@ -225,7 +239,7 @@ app.get('/movies/:title', (req, res) => {
 //READ- returns data about a genre by name/title
 app.get('/movies/genre/:genreName', (req, res) => {
     const { genreName } = req.params;
-    const genre = movies.find(movie => movie.Genre.Name === genreName).Genre;
+    const genre = movies.find( movie => movie.Genre.Name === genreName).Genre;
 
     if (genre) {
         res.status(200).json(genre);
@@ -235,7 +249,7 @@ app.get('/movies/genre/:genreName', (req, res) => {
 })
 
 //READ- returns data about a director
-app.get('/movies/directors/:directorName', (req, res) => {
+app.get('/movies/director/:directorName', (req, res) => {
     const { directorName } = req.params;
     const director = movies.find(movie => movie.Director.Name === directorName).Director;
 
